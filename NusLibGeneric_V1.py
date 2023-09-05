4e8dc5bc9c334ac53dde3244a1c58387cc5de5a0
realnus <42910526+realnus@users.noreply.github.com>
2023-09-05 14:39:20 +0300

    Add files via upload

--- NusLibGeneric_V1.py
+++ NusLibGeneric_V1.py
@@ -11,7 +11,6 @@ def AppendToTextFile_ThreadSafe_v1(FilePath,data):
         file1.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')  + " | " + data)
         file1.close()
 
-
 def PercentChange(current, previous):
     if current == previous:
         return 0
@@ -19,21 +18,3 @@ def PercentChange(current, previous):
         return (current - previous) / previous * 100.0
     except ZeroDivisionError:
         return 0
-
-
-def CalculateTimeDifference(StartDateTime, EndDateTime):
-    diff = EndDateTime - StartDateTime
-
-    days, seconds = diff.days, diff.seconds
-    hours = days * 24 + seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
-    milli_secs = diff.total_seconds() * 1000
-
-    return "days:" +  str(days) + " hours:" + str(hours) + " minutes:" + str(minutes) + " seconds:",str(seconds) + " milli_secs:" + str(milli_secs)
-
-
-def GetTotalCpuUsage():
-    import psutil 
-    cpu = psutil.cpu_percent(interval=5)
-    return cpu   
